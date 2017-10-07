29788cf74b736595592ffb45af5c13b7a74e7ef8
reflectOr <manaskashyaptech@gmail.com>
2017-10-07 19:37:15 +0530

    settings button working and app icon changed

--- src/app/src/main/java/in/asetalias/alias/MainActivity.java
+++ src/app/src/main/java/in/asetalias/alias/MainActivity.java
@@ -1,11 +1,9 @@
 package in.asetalias.alias;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -24,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -36,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
             return true;
         }
 
